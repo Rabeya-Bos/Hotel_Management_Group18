8bd883cfa64a1ff2a74f4c429a76cb7581eba85c
Tasnim Jahan Urmi <2230343@iub.edu.bd>
2026-03-31 15:26:21 +0600

    First Commit

---
+++ src/main/java/com/eror/hotelmanagementgroup18/urmi/Goal_1_recordSupplyController.java
@@ -0,0 +1,29 @@
+package com.eror.hotelmanagementgroup18.urmi;
+
+public class Goal_1_recordSupplyController
+{
+    @javafx.fxml.FXML
+    private TableColumn dateCol;
+    @javafx.fxml.FXML
+    private ComboBox itemComboBox;
+    @javafx.fxml.FXML
+    private TableColumn quantityCol;
+    @javafx.fxml.FXML
+    private DatePicker datePicker;
+    @javafx.fxml.FXML
+    private TextField quantityTF;
+    @javafx.fxml.FXML
+    private TableView supplyTV;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TableColumn itemCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void saveSupplyButtonOA(ActionEvent actionEvent) {
+    }
+}
