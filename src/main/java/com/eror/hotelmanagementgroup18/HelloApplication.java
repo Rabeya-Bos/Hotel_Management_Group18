aa6b42511efc00034aebdc6235225b3dda7ac541
arpitasaha30 <arpita.saha200002@gmail.com>
2026-03-31 13:17:46 +0600

    Initial version2

--- src/main/java/com/eror/hotelmanagementgroup18/HelloApplication.java
+++ src/main/java/com/eror/hotelmanagementgroup18/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene- 2_Assign Rooms to Guests.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene- 4-Approve Guest Check-In.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
