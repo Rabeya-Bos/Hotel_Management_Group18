850ba5aeb36a01ae35e5d9809be03dd97c01cc41
md-sumon-islam <mdsumon.islam20112002@gmail.com>
2026-04-08 09:08:48 +0600

    updated fxml

---
+++ src/main/java/com/eror/hotelmanagementgroup18/Sumon/User7Goal1Controller.java
@@ -0,0 +1,40 @@
+package com.eror.hotelmanagementgroup18.Sumon;
+
+import javafx.scene.control.*;
+
+public class User7Goal1Controller
+{
+    @javafx.fxml.FXML
+    private ComboBox floorCombo;
+    @javafx.fxml.FXML
+    private Button searchBtn;
+    @javafx.fxml.FXML
+    private TextField searchRoomIdField;
+    @javafx.fxml.FXML
+    private TableColumn colRate;
+    @javafx.fxml.FXML
+    private TableColumn colFloor;
+    @javafx.fxml.FXML
+    private Button addBtn;
+    @javafx.fxml.FXML
+    private TableColumn colRoomId;
+    @javafx.fxml.FXML
+    private TableView inventoryTable;
+    @javafx.fxml.FXML
+    private TextField categoryField;
+    @javafx.fxml.FXML
+    private TableColumn colType;
+    @javafx.fxml.FXML
+    private Button backBtn;
+    @javafx.fxml.FXML
+    private TextField rateField;
+    @javafx.fxml.FXML
+    private TextField addRoomIdField;
+    @javafx.fxml.FXML
+    private ComboBox typeCombo;
+    @javafx.fxml.FXML
+    private TableColumn colStatus;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
---
+++ src/main/java/com/eror/hotelmanagementgroup18/Sumon/User7Goal7Controller.java
@@ -0,0 +1,20 @@
+package com.eror.hotelmanagementgroup18.Sumon;
+
+import javafx.scene.control.TableView;
+
+import java.awt.*;
+
+public class User7Goal7Controller
+{
+    @javafx.fxml.FXML
+    private TableView inventoryTable;
+    @javafx.fxml.FXML
+    private Button backBtn;
+    @javafx.fxml.FXML
+    private TextField vendorId;
+    @javafx.fxml.FXML
+    private TextField stockLevel;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
