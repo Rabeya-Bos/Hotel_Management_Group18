7c8df3c0fb162b06fc097797a65988447233fe53
arpitasaha30 <arpita.saha200002@gmail.com>
2026-04-11 03:54:10 +0600

    scene6 code done

--- src/main/java/com/eror/hotelmanagementgroup18/HelloApplication.java
+++ src/main/java/com/eror/hotelmanagementgroup18/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("/com/eror/hotelmanagementgroup18/arpita/Goal5-EmergencyReport.fxml")
+                HelloApplication.class.getResource("/com/eror/hotelmanagementgroup18/arpita/Goal6-ShiftChangeRequest.fxml")
         );
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hotel Management!");
